8adfa90f0cb07f66c8b52ef46904acfbf0d8a250
João Melo <joao_melo@id.uff.br>
2016-06-18 18:04:18 -0300

    verificar_contido 1.0
    Função que verifica se um conjunto está contido em outro.
    
    Pensando em trocar o iffe por um switch, ou arrumar uma forma de fazer menos verificações.
    
    AVISO: a função ta mt tosca.

--- conjunto.c
+++ conjunto.c
@@ -9,10 +9,24 @@ int verificar (conjunto * c, int numero) {
 }
 //Verifica se um conjunto esta contido em outro. Retorna 1, caso verdadeiro e 0, caso falso.
 int verificar_contido (conjunto * ca, conjunto * cb) {
+    //trocar esses ifs por um switch, caso possivel.
+    //Precisa ser otimizada.
+    if ( (ca == NULL) && (cb == NULL) ) //Verfica se os dois conjuntos são vazios
+    else if (ca == NULL)  //Se ca for vazio e cb nao, ca esta contido em cb
+    else if (cb == NULL) //se cb for vazio e ca nao, ca nao esta contido em cb
+        return 0;
+    else { //se nenhum dos dois for vazio, verifica os elementos de cada um
+        while (ca != NULL) {
+            if ( verificar (cb, ca->numero) == 0)//verifica se tem algum elemento em ca que nao esta contido em cb
+                return 0;
+
+            ca = ca->prox;
+        }
+
+    }
 
     return 1;
 
-    return 0;
 }
 //Verifica se um conjunto contem outro. Retorna 0, caso falso e 1, caso verdadeiro.
 int verificar_contem (conjunto *ca, conjunto * cb) {
