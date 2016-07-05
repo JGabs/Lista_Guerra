ec3bd3449a6a3ffe061f73df1fbb288e39884fc7
João Melo <joao_melo@id.uff.br>
2016-07-05 12:43:58 -0300

    Versão 1.4 Release
    - Corrigido erro na função verificar_contem;
    - Adicionada uma nova verificação nas funções verficar_contem e verificar_contido, que verifica se um mesmo conjunto foi usado como os dois parametros da função;
    - Corrigidas pequenas disparidades na estética do código;

--- conjunto.c
+++ conjunto.c
@@ -10,8 +10,7 @@ int verificar(conjunto * c, int numero) {
 			return TRUE;
 		}
 		
-		c = c->prox;
-		
+		c = c->prox;	
 	}
 	
 	return FALSE;
@@ -20,7 +19,13 @@ int verificar(conjunto * c, int numero) {
 
 //Verifica se um conjunto esta contido em outro. Retorna TRUE, caso verdadeiro e FALSE, caso falso.
 int verificar_contido(conjunto * ca, conjunto * cb) {
-    /*
+    
+	//Verfifica se os dois ponteiros referenciam o mesmo conjunto.
+	if (ca == cb) {
+		return TRUE;
+	}
+	
+	/*
     *EXPLICACAO LONGA ABAIXO!
     *Se quiser seguir adiante com o codigo, pule e volte em caso de duvida.
     *===========================================================================================================================================
@@ -42,7 +47,6 @@ int verificar_contido(conjunto * ca, conjunto * cb) {
 
             ca = ca->prox;
         }
-
     }
     //Caso nao caia em nenhuma das verificacoes do caso FALSO, retorno e' TRUE.
     return TRUE;
@@ -50,35 +54,39 @@ int verificar_contido(conjunto * ca, conjunto * cb) {
 }
 
 //Verifica se um conjunto contem outro. Retorna 0, caso falso e 1, caso verdadeiro.
-int verificar_contem(conjunto *ca, conjunto * cb) {
-
-if ((ca == NULL) && (cb == NULL)) {     //Verifica se os dois conjuntos são vazios
+int verificar_contem(conjunto * ca, conjunto * cb) {
+	
+	//Verfifica se os dois ponteiros referenciam o mesmo conjunto.
+	if (ca == cb) {
+		return TRUE;
+	}
+	
+	if ((ca == NULL) && (cb == NULL)) { //Verifica se os dois conjuntos são vazios
+		return TRUE;
+	} else if (ca == NULL) { //se ca for vazio e cb nao, ca nao contem cb
 		return FALSE;
-	} else if (cb == NULL) {    //se ca for vazio e cb nao, ca nao contem cb
-		return FALSE;
-    } else if (ca == NULL) {     //se cb for vazio e ca nao, ca contem cb
+    } else if (cb == NULL) { //se cb for vazio e ca nao, ca contem cb
         return TRUE;
-    } else {    //se nenhum dos dois for vazio, verifica os elementos de cada um
+    } else { //se nenhum dos dois for vazio, verifica os elementos de cada um
 		while (cb != NULL) {
-			if (verificar(ca,cb->numero) == FALSE) {
+			if (verificar(ca,cb->numero) == FALSE){
 				return FALSE;
 			}
-
-			cb = cb->prox;
-		}
+			
+			cb = cb->prox;	
+		}	
 	}
 
 	return TRUE;
-
 }
 
 //Insere um elemento no conjunto, caso ja nao exista uma ocorrencia do mesmo no conjunto.
 conjunto * inserir(conjunto * c, int numero) {
 
-	if (c != NULL){ // verifica se o conjunto é nulo
-		if (verificar(c, numero) == TRUE){ // se não for, verifica se existe uma ocorrência do número 
+	if (c != NULL) { // verifica se o conjunto é nulo
+		if (verificar(c, numero) == TRUE) { // se não for, verifica se existe uma ocorrência do número 
 			return c;
-		}
+		}	
 	}
 
 	conjunto * novo = (conjunto *) malloc(sizeof(conjunto));
@@ -87,8 +95,7 @@ conjunto * inserir(conjunto * c, int numero) {
 	novo->prox = c;
 	c = novo;
 	
-	return c;
-	
+	return c;	
 }
 
 //Remove um elemento do conjunto.
@@ -112,45 +119,40 @@ conjunto * remover_elemento(conjunto * c, int numero) {
 	}
 	free(p);
 	
-    return c;
-	
+    return c;	
 }
 
 //Soma dois conjuntos.
- conjunto * somar (conjunto * ca, conjunto * cb) {
-	
+conjunto * somar(conjunto * ca, conjunto * cb) {
 	conjunto * c = NULL;
 	
-	while (ca != NULL){//insere todos os elementos de ca em c
-		c = inserir (c, ca->numero);
+	while (ca != NULL) {//insere todos os elementos de ca em c
+		c = inserir(c, ca->numero);
 		ca = ca->prox;
 	}
 	
-	while (cb != NULL){//insere todos os elementos de cb em c
-		c = inserir (c, cb->numero);
+	while (cb != NULL) {//insere todos os elementos de cb em c
+		c = inserir(c, cb->numero);
 		cb = cb->prox;
 	}
-	
-	
+		
 	return c;
- }
+}
  
- //Subtrai um conjunto de outro.
- conjunto * subtrair (conjunto * ca, conjunto * cb) {
-	
+//Subtrai um conjunto de outro.
+conjunto * subtrair(conjunto * ca, conjunto * cb) {
 	conjunto * c = NULL;
 	
 	//clona a lista ca para poder deletar os elementos sem perder a lista anterior.
-	c = somar (ca, NULL);
+	c = somar(ca, NULL);
 	
-	while (cb != NULL){//remove os elementos de cb em ca.
-		c = remover_elemento (c, cb->numero);
+	while (cb != NULL)	{	//remove os elementos de cb em ca.
+		c = remover_elemento(c, cb->numero);
 		cb = cb->prox;
 	}
 	
 	return c;
- }
- 
+}
 
 //Realiza a uniao de dois conjuntos em um novo.
 conjunto * unir(conjunto * ca, conjunto * cb) {
@@ -158,24 +160,23 @@ conjunto * unir(conjunto * ca, conjunto * cb) {
 	// A U B = (A - B) + B
 	
 	conjunto * uniao = NULL;
-	conjunto * temp = NULL;//lista temporária para poder realizar o cálculo.
+	conjunto * temp = NULL;	//lista temporária para poder realizar o cálculo.
 	
-	temp = subtrair (ca, cb);
-	uniao = somar (temp,cb);
+	temp = subtrair(ca, cb);
+	uniao = somar(temp, cb);
 	
-	/*Com o tratamento feito pela função inserir a poderiamos usar apenas a seguinte linha:
-	 *
-	 *uniao = somar (ca, cb);
-	 *poém qualquer alteração em inserir iria danificar a função, por isso opitei pela fórmula da teoria dos conjuntos. 
+	/*
+	*Com o tratamento feito pela função inserir a poderiamos usar apenas a seguinte linha:
+	*
+	*uniao = somar (ca, cb);
+	*poém qualquer alteração em inserir iria danificar a função, por isso opitei pela fórmula da teoria dos conjuntos. 
 	*/
 	
-	while(temp!=NULL){//limpa a lista temporária
-		temp = remover_elemento (temp, temp->numero);
+	while (temp != NULL) {	//limpa a lista temporária
+		temp = remover_elemento(temp, temp->numero);
 	}
 	
     return uniao;
-	
-	
 }
 
 //Realiza a intersecao de dois conjunto em um novo.
@@ -184,15 +185,14 @@ conjunto * intersec(conjunto * ca, conjunto * cb) {
 	//A ∩ B = A - (A - B)
 	
 	conjunto * intersecao = NULL;
-	conjunto * temp = NULL;//lista temporária para poder realizar o cálculo.
+	conjunto * temp = NULL;	//lista temporária para poder realizar o cálculo.
 	
-	temp= subtrair (ca, cb);
-	intersecao = subtrair (ca, temp);
+	temp = subtrair(ca, cb);
+	intersecao = subtrair(ca, temp);
 	
-	while(temp!=NULL){//limpa a lista temporária
+	while(temp != NULL){	//limpa a lista temporária
 		temp = remover_elemento (temp, temp->numero);
 	}
 	
     return intersecao;
-
 }
